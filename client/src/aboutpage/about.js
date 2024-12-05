77d3fc8a5288ff0e7b8659447753f6d97a586343
Parth Choudhary <parthchoudhary4372@gmail.com>
2024-12-05 20:16:42 +0530

    add gsap animation

--- client/src/aboutpage/about.js
+++ client/src/aboutpage/about.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
-function about() {
+const Page1 = () => {
+  const pageRef = useRef();
+
+  useEffect(() => {
+    gsap.fromTo(
+      pageRef.current,
+      { opacity: 0, y: 50 },
+      { opacity: 1, y: 0, duration: 1, ease: "power3.out" }
+    );
+  }, []);
+
   return (
-    <>
-   
-    </>
-  )
-}
+    <div ref={pageRef} style={{ height: "100vh", background: "#ff6f61" }}>
+      <h1>Page 1</h1>
+      <p>Welcome to Page 1!</p>
+    </div>
+  );
+};
 
-export default about
+export default Page1;
